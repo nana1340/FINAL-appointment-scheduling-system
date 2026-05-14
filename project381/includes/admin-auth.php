f9d38ed242d270e3270070228ebf63e252bf44c1
Layan Aljohani <layan19780@gmail.com>
2026-05-14 22:07:36 +0300

    Delete project381 directory

--- project381/includes/admin-auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-require_once __DIR__ . "/security.php";
-
-if (!isset($_SESSION['role']) || $_SESSION['role'] != 'admin') {
-    header("Location: ../login.php");
-    exit();
-}
-?>
